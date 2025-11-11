c397d603795fbebd7d505955770cf3bf5910cdbc
OSCR <oscarge10.oge@gmail.com>
2025-11-11 09:05:43 +0100

    V.0.6.1 script de movimiento horizontal creado

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float maxSpeed = 6.0f;
+    public float moveDirection;
+    public bool facingRight;
+    private Rigidbody rigidbody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.linearVelocity = new Vector2(moveDirection * maxSpeed, rigidbody.linearVelocity.y);
+    }
+
+}
